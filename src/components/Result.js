4bdc14cf9886e44ca39f4d896b9bd7eb01ab710a
StuartCharters <charters.stuart@gmail.com>
2018-08-28 14:19:57 +0100

    can get name and sprite in the browser

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const Result = (props) => {
+  if(!props.selectedPokemon) return null;
   return (
-    'hello'
+    <div>
+      <h2>{props.selectedPokemon.name}</h2>
+      <img src={props.selectedPokemon.sprite}></img>
+    </div>
   );
 }
 
