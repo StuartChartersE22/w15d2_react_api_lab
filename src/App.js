0bf84761363cab7c983c67c1e59b873e5b8f891c
StuartCharters <charters.stuart@gmail.com>
2018-08-28 13:16:10 +0100

    setup pokebox file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PokeBox from './containers/PokeBox.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      
+      <PokeBox/>
     );
   }
 }
---
+++ src/containers/PokeBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class PokeBox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pokemons:[],
+      selectedPokemon: null
+    }
+  }
+
+  render(){
+    return ;
+  }
+
+}
+
+export default PokeBox;
