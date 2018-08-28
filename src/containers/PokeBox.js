fb5e8e3f71da94b09b1cd1a1b356773065f78482
StuartCharters <charters.stuart@gmail.com>
2018-08-28 13:31:05 +0100

    getting all 151 gen 1 pokemon

--- src/containers/PokeBox.js
+++ src/containers/PokeBox.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import SelectBox from '../components/SelectBox.js'
+import Result from '../components/Result.js'
 
 class PokeBox extends React.Component {
   constructor(props) {
@@ -9,8 +11,24 @@ class PokeBox extends React.Component {
     }
   }
 
+  componentDidMount() {
+    const url = 'https://pokeapi.co/api/v2/generation/1/'
+    fetch(url)
+     .then((res) => {
+       return res.json();
+     })
+     .then((gen1Info) => {
+       const pokemons = gen1Info.pokemon_species
+       this.setState({pokemons: pokemons})
+     })
+  }
+
   render(){
-    return ;
+    return (
+      <div>
+        
+      </div>
+    );
   }
 
 }
